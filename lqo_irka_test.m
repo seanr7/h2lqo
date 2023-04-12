81765a10330c287f0784483fbbe067455109d9ab
Sean Reiter <seanr7@vt.edu>
2023-04-12 13:51:43 -0400

    added rand example

--- lqo_irka_test.m
+++ lqo_irka_test.m
@@ -1,7 +1,9 @@
-% Test LQO_irka
+% Test lqo-irka
 clc
 clear all
 close all
+
+%% example from lqo Overleaf doc
 
 A = -diag([1,2,3,4]);    b = [1;1;2;2];  c = [2,2,3,3];
 K = [20, -10, 0, 0;
@@ -23,13 +25,17 @@ H2r = @(s1, s2) br'*((s1*Ir-Ar)'\Kr)*((s2*Ir-Ar)\br);
 
 l1 = lambda(1); l2 = lambda(2);
 fprintf('1st-order optimality conditions, 2r in total')
-[H1(-l1)-H1r(-l1), H1(-l2)-H1r(-l2)]
+for i = 1:r
+    [H1(-lambda(1)) - H1r(-lambda(1))]
+end
 
 fprintf('2nd-order optimality conditions, r^2 in total')
-[H2(-l1,-l1)-H2r(-l1,-l1)]
-[H2(-l1,-l2)-H2r(-l1,-l2)]
-[H2(-l2,-l1)-H2r(-l2,-l1)]
-[H2(-l2,-l2)-H2r(-l2,-l2)]
+for i = 1:r
+    for j = 1:r
+        [H2(-lambda(i),-lambda(j)) - H2r(-lambda(i),-lambda(j))]
+    end
+    
+end
 
 % Derivatives?
 H1_prime = @(s) -c*((s*I-A)\((s*I-A)\b));
@@ -40,6 +46,23 @@ H2r_s1prime = @(s1,s2) -br'*((s1*Ir-Ar)'\((s1*Ir-Ar)'\Kr))*((s2*Ir-Ar)\br);   % 
 
 H2_s2prime = @(s1,s2) -b'*((s1*I-A)'\K)*((s2*I-A)\((s2*I-A)\b)); % Partial deriv w.r.t s2
 H2r_s2prime = @(s1,s2) -br'*((s1*Ir-Ar)'\Kr)*((s2*Ir-Ar)\((s2*Ir-Ar)\br)); % Partial deriv w.r.t s2
+
+% So, strictly derivatives are not interpolated... But are the mixed
+% conditions satisfied? 
+
+fprintf('Mixed linear + quadratic optimality conditions, 2r in total')
+
+for i = 1:r
+    ROpart = 2*phi(i)*H1r_prime(-lambda(i));
+    FOpart = 2*phi(i)*H1_prime(-(lambda(i)));
+    for j = 1:r
+        ROpart = ROpart + kappa(i,j)*H2r_s1prime(-lambda(i),-lambda(j)) + ...
+            kappa(j,i)*H2r_s2prime(-lambda(j),-lambda(i));
+        FOpart = FOpart + kappa(i,j)*H2_s1prime(-lambda(i),-lambda(j)) + ...
+            kappa(j,i)*H2_s2prime(-lambda(j),-lambda(i));
+    end
+    [FOpart - ROpart]
+end
 
 % fprintf('Linear derivative interpolation?')
 % [H1_prime(-l1)-H1r_prime(-l1), H1_prime(-l2)-H1r_prime(-l2)]
@@ -52,26 +75,69 @@ H2r_s2prime = @(s1,s2) -br'*((s1*Ir-Ar)'\Kr)*((s2*Ir-Ar)\((s2*Ir-Ar)\br)); % Par
 % [H2_s2prime(-l1,-l2)-H2r_s2prime(-l1,-l2)]
 % [H2_s2prime(-l2,-l1)-H2r_s2prime(-l2,-l1)]
 
+%% a random example...
+n = 10;
+r = 2;
+
+% TODO: Somewhere under the hood, I must be assuming implicitly that A is a
+% diagonal matrix at the start, because the code fails if that is not the
+% case...
+A = diag(rand(n,1));  b = rand(n,1);  c = rand(1,n);  K = rand(n,n);
+K = (K+K')/2;
+
+lambda_prev = rand(r,1);  phi_prev = rand(r,1);   kappa_prev = rand(r,r);
+[Ar, br, cr, Kr, lambda, phi, kappa] = lqo_irka(A, b, c, K, lambda_prev, phi_prev, kappa_prev, 100, 10e-12, 1);
+
+% Are interpolation conditions satisfied?
+I = eye(n,n);   Ir = eye(r,r);
+H1 = @(s) c*((s*I-A)\b); 
+H2 = @(s1, s2) b'*((s1*I-A)'\K)*((s2*I-A)\b);
+
+H1r = @(s) cr*((s*Ir-Ar)\br); 
+H2r = @(s1, s2) br'*((s1*Ir-Ar)'\Kr)*((s2*Ir-Ar)\br); 
+
+l1 = lambda(1); l2 = lambda(2);
+fprintf('1st-order optimality conditions, 2r in total')
+for i = 1:r
+    [H1(-lambda(1)) - H1r(-lambda(1))]
+end
+
+fprintf('2nd-order optimality conditions, r^2 in total')
+for i = 1:r
+    for j = 1:r
+        [H2(-lambda(i),-lambda(j)) - H2r(-lambda(i),-lambda(j))]
+    end
+    
+end
+
+% Derivatives?
+H1_prime = @(s) -c*((s*I-A)\((s*I-A)\b));
+H1r_prime = @(s) -cr*((s*Ir-Ar)\((s*Ir-Ar)\br));
+
+H2_s1prime = @(s1,s2) -b'*((s1*I-A)'\((s1*I-A)'\K))*((s2*I-A)\b); % Partial deriv w.r.t s1
+H2r_s1prime = @(s1,s2) -br'*((s1*Ir-Ar)'\((s1*Ir-Ar)'\Kr))*((s2*Ir-Ar)\br);   % Partial deriv w.r.t s1
+
+H2_s2prime = @(s1,s2) -b'*((s1*I-A)'\K)*((s2*I-A)\((s2*I-A)\b)); % Partial deriv w.r.t s2
+H2r_s2prime = @(s1,s2) -br'*((s1*Ir-Ar)'\Kr)*((s2*Ir-Ar)\((s2*Ir-Ar)\br)); % Partial deriv w.r.t s2
+
 % So, strictly derivatives are not interpolated... But are the mixed
 % conditions satisfied? 
 
 fprintf('Mixed linear + quadratic optimality conditions, 2r in total')
 
-ROpart1 = 2*phi(1)*H1r_prime(-l1) + ...
-    kappa(1,1)*H2r_s1prime(-l1,-l1) + kappa(1,2)*H2r_s1prime(-l1,-l2) + ...
-    kappa(1,1)*H2r_s2prime(-l1,-l1) + kappa(2,1)*H2r_s2prime(-l2,-l1);
-FOpart1 = 2*phi(1)*H1_prime(-l1) + ... 
-    kappa(1,1)*H2_s1prime(-l1,-l1) + kappa(1,2)*H2_s1prime(-l1,-l2) + ...
-    kappa(1,1)*H2_s2prime(-l1,-l1) + kappa(2,1)*H2_s2prime(-l2,-l1);
-[ROpart1 - FOpart1]
+for i = 1:r
+    ROpart = 2*phi(i)*H1r_prime(-lambda(i));
+    FOpart = 2*phi(i)*H1_prime(-(lambda(i)));
+    for j = 1:r
+        ROpart = ROpart + kappa(i,j)*H2r_s1prime(-lambda(i),-lambda(j)) + ...
+            kappa(j,i)*H2r_s2prime(-lambda(j),-lambda(i));
+        FOpart = FOpart + kappa(i,j)*H2_s1prime(-lambda(i),-lambda(j)) + ...
+            kappa(j,i)*H2_s2prime(-lambda(j),-lambda(i));
+    end
+    [FOpart - ROpart]
+end
 
-ROpart2 = 2*phi(2)*H1r_prime(-l2) + ...
-    kappa(2,1)*H2r_s1prime(-l2,-l1) + kappa(2,2)*H2r_s1prime(-l2,-l2) + ...
-    kappa(1,2)*H2r_s2prime(-l1,-l2) + kappa(2,2)*H2r_s2prime(-l2,-l2);
-FOpart2 = 2*phi(2)*H1_prime(-l2) + ...
-    kappa(2,1)*H2_s1prime(-l2,-l1) + kappa(2,2)*H2_s1prime(-l2,-l2) + ...
-    kappa(1,2)*H2_s2prime(-l1,-l2) + kappa(2,2)*H2_s2prime(-l2,-l2);
-[ROpart2 - FOpart2]
+%% lqo-irka
 
 function [Ar, br, cr, Kr, lambda, phi, kappa] = lqo_irka(A, b, c, K, ...
     lambda_prev, phi_prev, kappa_prev, itermax, eps, plot_conv)
@@ -97,7 +163,7 @@ function [Ar, br, cr, Kr, lambda, phi, kappa] = lqo_irka(A, b, c, K, ...
 % 
 % Outputs:
 % :param (Ar, br, cr, Kr): ((r,r), (r,1), (1,r), (r,r)) State-space 
-%                           realization of the LQO-IRAK ROM.
+%                           realization of the LQO-IRKA ROM.
 % :param lambda          : Converged interpolation points, (r,1) array
 % :param phi             : Converged 1st-order residues, (r,1) array
 % :param kappa           : Converged 2nd-order residues, (r,r) array
