8df0466f85e2497ad1cc78c75092ceafc95365d2
Sean Reiter <seanr7@vt.edu>
2023-10-31 17:05:41 -0400

    Update lqo script

--- benchmarks/plateTVA/RUNME_lqo.m
+++ benchmarks/plateTVA/RUNME_lqo.m
@@ -2,7 +2,7 @@
 % Author: Sean Reiter (seanr7@vt.edu)
 clear
 close all
-%%
+%% 
 % 'rms' - evaluate root mean square z-displacement of all nodes on the
 %         plate surface
 
@@ -17,6 +17,7 @@ close all
 load('plateTVA_n201900m1q28278_full')
 n_nodes = full(sum(sum(C)));
 
+%% Convert plate model to FO (first-order) from SO (second-order)
 % Model is given in SO-form
 % Necessarily, need to conver to FO to do LQO_IRKA for now
 [n, ~] = size(M);
@@ -30,6 +31,33 @@ A_qo(1:n, 1:n) = speye(n); % (1, 1) block of A_qo
 A_qo(n+1:2*n, 1:n) = -K;  % (2, 1) block is -damping matrix
 A_qo(n+1:2*n, n+1:2*n) = -E; % (2, 2) block is -stiffness matrix
 
-% Our `M' matrix (i.e., the quadratic output matrix) is just the (n_nodes x n_nodes) identity
-M_qo = eye(n_nodes, n_nodes);
+B_qo = spalloc(2*n, 1, nnz(B)); % B_qo = [0; B];
+% No scalar output in this example; only QO
 
+% Our `M' matrix (i.e., the quadratic output matrix) is just the (n_nodes x n_nodes) identity
+M_qo = speye(n_nodes);
+
+%% Sample H2(s1, s2) (QO-tf)
+% frequencies to sample at (s) are given in '*.mat' file 
+
+recompute = true;
+if recompute == true
+    res = zeros(1,length(s));
+    for ii=1:length(s)
+        fprintf('Frequency step %d, f=%.2f Hz ... ',ii,imag(s(ii))/2/pi)
+        tic
+        tmp = (s(ii) * E_qo - A_qo) \ B_qo;
+        res(ii) = sqrt(tmp' * M_qo * tmp) / n_nodes; % Q: So really, we want sqrt(H_2(s(ii), s(ii))/n_nodes ? (Just to put it in my language..)
+        fprintf('finished in %.2f s\n',toc)
+    end
+end
+
+f = imag(s)/2/pi;
+mag = 10*log10(abs(res)/1e-9);
+
+figure('name','Transfer function')
+plot(f,mag)
+xlabel('Frequency [Hz]')
+ylabel('Magnitude [dB]')
+
+%% Do via LQO-IRKA
