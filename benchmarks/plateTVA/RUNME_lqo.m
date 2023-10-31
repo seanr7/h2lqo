82ec9f100c98afc322c71c666a62c1132e808314
Sean Reiter <seanr7@vt.edu>
2023-10-31 16:46:19 -0400

    update lqo script

--- benchmarks/plateTVA/RUNME_lqo.m
+++ benchmarks/plateTVA/RUNME_lqo.m
@@ -14,5 +14,22 @@ close all
 % outputs. Instead, treat as an LQO model, and compute these evaluations
 % directly
 
-load('plateTVA_n201900m1q28278')
+load('plateTVA_n201900m1q28278_full')
 n_nodes = full(sum(sum(C)));
+
+% Model is given in SO-form
+% Necessarily, need to conver to FO to do LQO_IRKA for now
+[n, ~] = size(M);
+
+E_qo = spalloc(2*n, 2*n, nnz(M) + n); % Descriptor matrix; E_qo = [I, 0: 0, M]
+E_qo(1:n, 1:n) = speye(n); % (1, 1) block
+E_qo(n+1:2*n, n+1:2*n) = M; % (2, 2) block is mass matrix
+
+A_qo = spalloc(2*n, 2*n, nnz(K) + nnz(E) + n);  % A_qo = [0, I; -K, -E]
+A_qo(1:n, 1:n) = speye(n); % (1, 1) block of A_qo
+A_qo(n+1:2*n, 1:n) = -K;  % (2, 1) block is -damping matrix
+A_qo(n+1:2*n, n+1:2*n) = -E; % (2, 2) block is -stiffness matrix
+
+% Our `M' matrix (i.e., the quadratic output matrix) is just the (n_nodes x n_nodes) identity
+M_qo = eye(n_nodes, n_nodes);
+
