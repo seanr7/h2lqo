c1ba5afe67b3eb7501eb8d0940cc297b43cab8a5
Sean Reiter <seanr7@vt.edu>
2024-02-11 21:53:55 -0500

    benchmarks RUNME

---
+++ RUNME_benchmarks.m
@@ -0,0 +1,188 @@
+%%
+% Author: Sean Reiter (seanr7@vt.edu)
+clear
+close all
+%% 
+% 'rms' - evaluate root mean square z-displacement of all nodes on the
+%         plate surface
+
+% Objective: frequency-domain rms evaluations of plate model in
+%            `plateTVA_n201900m1q28278.mat'. Available at
+%            https://zenodo.org/record/7671686
+
+% To compute rms `output', need to simulate a SIMO model, with 28278
+% outputs. Instead, treat as an linear quadratic output system, 
+% and compute rms evaluations directly
+
+addpath('drivers/')
+addpath('data/')
+fprintf('Loading plateTVA model...\n')
+load('data/plateTVA_n201900m1q28278_fo')
+n_nodes = full(sum(sum(C)));
+
+%% 
+% Convert plate model to FO (first-order) from SO (second-order)
+% Model is given in SO-form
+% Necessarily, need to conver to FO to do LQO_IRKA for now
+fprintf('Converting 2nd-order LTI system to a 1st-order LQO system\n')
+tic
+[n, ~] = size(M);
+
+E_qo = spalloc(2*n, 2*n, nnz(M) + n); % Descriptor matrix; E_qo = [I, 0: 0, M]
+E_qo(1:n, 1:n) = speye(n); % (1, 1) block
+E_qo(n+1:2*n, n+1:2*n) = M; % (2, 2) block is (sparse) mass matrix
+
+A_qo = spalloc(2*n, 2*n, nnz(K) + nnz(E) + n);  % A_qo = [0, I; -K, -E]
+A_qo(1:n, n+1:2*n) = speye(n); % (1, 2) block of A_qo
+A_qo(n+1:2*n, 1:n) = -K;  % (2, 1) block is -damping matrix
+A_qo(n+1:2*n, n+1:2*n) = -E; % (2, 2) block is -stiffness matrix
+
+B_qo = spalloc(2*n, 1, nnz(B)); % B_qo = [0; B];
+B_qo(n+1:2*n, :) = B;
+% No scalar output in this example; only QO
+
+% Our `M' matrix (i.e., the quadratic output matrix) is C' * C
+Q_qo = spalloc(2*n, 2*n, nnz(C' * C));
+Q_qo(1:n, 1:n) = C' * C; 
+fprintf('1st-order LQO realization built in %.2f s\n',toc)
+
+%% 
+% Simulate full-order model 
+% 250 frequencies to sample at from 0hz - 250hz (s) are given in '*.mat' file 
+% Instead, use 500 frequences from 0hz - 500hz
+
+s = 1i*linspace(1,2*pi*501, 501);% Comment out to keep original freq range
+
+% recompute = true;
+recompute = false;
+if recompute == true
+    fprintf('Beginning full-order simulation. Estimated time of completion is %.2f s\n', 250*15.72)
+    overall_start = tic;
+    res = zeros(1,length(s));
+    for ii=1:length(s)
+        fprintf('Frequency step %d, f=%.2f Hz ... ',ii,imag(s(ii))/2/pi)
+        current_iter = tic;
+        tmp = (s(ii) * E_qo - A_qo) \ B_qo;
+        % res(ii) is H2(s(ii), s(ii)) = sqrt(((s(ii) * E_qo - A_qo)') \ Q_qo ((s(ii) * E_qo - A_qo) \ eye(n, n)))
+        % i.e., sqrt() of quadratic-output transfer function
+        res(ii) = sqrt((tmp' * Q_qo * tmp) / n_nodes); % Q: So really, we want sqrt(H_2(s(ii), s(ii))/n_nodes ? (Just to put it in my language..)
+        fprintf('Iteration of full-order simulation finished in %.2f s\n',toc(current_iter))
+    end
+    fprintf('Full-order simulation finished; total time of completion is %.2f s\n', toc(overall_start))
+    f = imag(s)/2/pi;
+    mag = 10*log10(abs(res)/1e-9);
+    frpintf('Saving full-order simulation data\n')
+    filename = 'FOsim_data.mat';
+    save(filename,'res','f','mag')
+    movefile FOsim_data.mat data/FOsim_data.mat
+
+else
+    fprintf('Not re-running the full-order simulation; loading saved data from file FOSIM_data.mat\n')
+    % load('FOSIM_data.mat')
+end
+
+% figure('name','Transfer function')
+% plot(f,mag)
+% xlabel('Frequency [Hz]')
+% ylabel('Magnitude [dB]')
+
+%% 
+% Now, compute LQO-ROM via 
+%   1. Linfty + Galerkin projection
+%   2. Linfty + Petrov-Galerkin projection
+%   3. Avg + Galerkin projection
+%   4. Avg + Petrov-Galerkin projection
+
+
+shifts = 1i*linspace(1,2*pi*251, 250);% Comment out to keep original freq rang
+r = 25; % Order
+fprintf('1. Computing LQO-ROM via Linfty sampling and Galerkin projection\n')
+opts.compression = 'Linfty';
+opts.proj = 'g';
+[Wprim, Vprim, Worth, Vorth, Hshifts_r25_Linfty_g, pW, pV, opts] = interpolatory_solves(E_qo, A_qo, B_qo, Q_qo, shifts, r, opts);
+% Compute LQO-ROM
+E_qo_r25_Linfty_g = Worth'*E_qo*Vorth; A_qo_r25_Linfty_g = Worth'*A_qo*Vorth; 
+Q_qo_r25_Linfty_g = Vorth'*Q_qo*Vorth; B_qo_r25_Linfty_g = Worth'*B_qo;
+
+
+filename = 'plateTVAlqo_r25_Linfty_g.mat';
+save(filename, 'E_qo_r25_Linfty_g', 'A_qo_r25_Linfty_g', 'B_qo_r25_Linfty_g', 'Q_qo_r25_Linfty_g', 'Hshifts_r25_Linfty_g', 'opts', ...
+    'pW', 'pV', 'Worth', 'Vorth', 'Wprim', 'Vprim') 
+movefile plateTVAlqo_r25_Linfty_g.mat data/plateTVAlqo_r25_Linfty_g.mat
+
+
+%%
+shifts = 1i*linspace(1,2*pi*251, 250);% Comment out to keep original freq rang
+r = 25; % Order
+fprintf('2. Computing LQO-ROM via Linfty sampling and Petrov-Galerkin projection\n')
+opts.compression = 'Linfty';
+opts.proj = 'pg';
+[Wprim, Vprim, Worth, Vorth, Hshifts_r25_Linfty_pg, pW, pV, opts] = interpolatory_solves(E_qo, A_qo, B_qo, Q_qo, shifts, r, opts);
+% Compute LQO-ROM
+E_qo_r25_Linfty_pg = Worth'*E_qo*Vorth; A_qo_r25_Linfty_pg = Worth'*A_qo*Vorth; 
+Q_qo_r25_Linfty_pg = Vorth'*Q_qo*Vorth; B_qo_r25_Linfty_pg = Worth'*B_qo;
+
+
+filename = 'plateTVAlqo_r25_Linfty_pg.mat';
+save(filename, 'E_qo_r25_Linfty_pg', 'A_qo_r25_Linfty_pg', 'B_qo_r25_Linfty_pg', 'Q_qo_r25_Linfty_pg', 'Hshifts_r25_Linfty_pg', 'opts', ...
+    'pW', 'pV', 'Worth', 'Vorth', 'Wprim', 'Vprim') 
+movefile plateTVAlqo_r25_Linfty_pg.mat data/plateTVAlqo_r25_Linfty_pg.mat
+
+%%
+shifts = 1i*linspace(1,2*pi*251, 250);% Comment out to keep original freq rang
+r = 25; % Order
+fprintf('3. Computing LQO-ROM via avg (pivoted QR) sampling and Galerkin projection\n')
+opts.compression = 'avg';
+opts.proj = 'g';
+[Wprim, Vprim, Worth, Vorth, Hshifts_r25_avg_g, pW, pV, opts] = interpolatory_solves(E_qo, A_qo, B_qo, Q_qo, shifts, r, opts);
+% Compute LQO-ROM
+E_qo_r25_avg_g = Worth'*E_qo*Vorth; A_qo_r25_avg_g = Worth'*A_qo*Vorth; 
+Q_qo_r25_avg_g = Vorth'*Q_qo*Vorth; B_qo_r25_avg_g = Worth'*B_qo;
+
+
+filename = 'plateTVAlqo_r25_avg_g.mat';
+save(filename, 'E_qo_r25_avg_g', 'A_qo_r25_avg_g', 'B_qo_r25_avg_g', 'Q_qo_r25_avg_g', 'Hshifts_r25_avg_g', 'opts', ...
+    'pW', 'pV', 'Worth', 'Vorth', 'Wprim', 'Vprim')  
+movefile plateTVAlqo_r25_avg_g.mat data/plateTVAlqo_r25_avg_g.mat
+
+%%
+shifts = 1i*linspace(1,2*pi*251, 250);% Comment out to keep original freq rang
+r = 25; % Order
+fprintf('4. Computing LQO-ROM via avg (pivoted QR) sampling and Petrov-Galerkin projection\n')
+opts.compression = 'avg';
+opts.proj = 'g';
+[Wprim, Vprim, Worth, Vorth, Hshifts_r25_avg_pg, pW, pV, opts] = interpolatory_solves(E_qo, A_qo, B_qo, Q_qo, shifts, r, opts);
+% Compute LQO-ROM
+E_qo_r25_avg_pg = Worth'*E_qo*Vorth; A_qo_r25_avg_pg = Worth'*A_qo*Vorth; 
+Q_qo_r25_avg_pg = Vorth'*Q_qo*Vorth; B_qo_r25_avg_pg = Worth'*B_qo;
+
+
+filename = 'plateTVAlqo_r25_avg_pg.mat';
+save(filename, 'E_qo_r25_avg_pg', 'A_qo_r25_avg_pg', 'B_qo_r25_avg_pg', 'Q_qo_r25_avg_pg', 'Hshifts_r25_avg_pg', 'opts', ...
+    'pW', 'pV', 'Worth', 'Vorth', 'Wprim', 'Vprim') 
+movefile plateTVAlqo_r25_avg_pg.mat data/plateTVAlqo_r25_avg_pg.mat
+
+
+% %%
+% % Now, do reduced-order simulations
+% 
+% fprintf('Beginning reduced-order simulation\n')
+% overall_start = tic;
+% 
+% res_ro = zeros(1,length(s));
+% for ii=1:length(s)
+%     fprintf('Frequency step %d, f=%.2f Hz ... ',ii,imag(s(ii))/2/pi)
+%     current_iter = tic;
+%     tmp = (s(ii) * E_qo_r - A_qo_r) \ B_qo_r;
+%     res_ro(ii) = sqrt((tmp' * Q_qo_r * tmp) / n_nodes);
+%     fprintf('Iteration of reduced-order simulation finished in %.2f s\n',toc(current_iter))
+% end
+% fprintf('Reduced-order simulation finished; time of completion is %.2f s/n', toc(overall_start))
+% 
+% f_ro = imag(s)/2/pi;
+% mag_ro = 10*log10(abs(res_ro)/1e-9);
+% 
+% fprintf('Saving reduced-order simulation data')
+% filename = 'ROsim_plateTVA_r25_lqoirka.mat';
+% movefile ROsim_plateTVA_r25_lqoirka.mat data/ROsim_plateTVA_r25_lqoirka.mat
+% save(filename,'res_ro','f_ro','mag_ro')
