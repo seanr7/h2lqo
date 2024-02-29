d532c24bc4a9b35991095641f052d1a62747f98f
Sean Reiter <seanr7@vt.edu>
2024-02-29 08:04:38 -0500

    reorg

---
+++ ReiW24/drivers/interpolatory_solves.m
@@ -0,0 +1,186 @@
+function [Wprim, Vprim, Worth, Vorth, H_shifts, pW, pV, opts] = interpolatory_solves(E, A, B, Q, shifts, r, opts)
+% Author: Sean Reiter (seanr7@vt.edu)
+
+% Function to compute linear solves for interpolatory model-order reduction
+% of linear quadratic-output (LQO) dynamical systems. `Primitive' bases are 
+% computed via linear solves at the inputted shifts; projection matrices 
+% are compressed according to input ops.
+
+% Inputs:
+% @param E, A, B, C, Q: Parameters of full-order LQO model
+% @param shifts:        Complex frequencies at which to interpolate the FOM
+% @param opts:          Struct containing input opts
+
+% Outputs:
+% @param Wprim:    `Primitive' left model reduction basis; contruction
+%                  according to opts.proj
+% @param Vprim:    `Primitive' right model reduction basis; contruction
+%                  according to opts.proj
+% @param Worth:    `Compressed' left model reduction basis; contruction
+%                  according to opts.compression
+% @param Vorth:    `Compressed' left model reduction basis; contruction
+%                  according to opts.compression
+% @param H_shifts: The quadratic-output transfer function evaluated at the
+%                  inputted shifts
+% @param: pV, pW:  Indices of shifts included in projection subspaces;
+%                  outputted for the sake of checking interpolatory cons
+
+% Opts:
+% @param compression:   How to compute Wroth, Vrorth, from Wrprim, Vrprim
+%                       Options
+%                           'Linfty': Build orth bases by selectiong r <= q 
+%                                     primitive basis vectors via a greedy
+%                                     selection; subsequent vectors are
+%                                     selected at shifts whre the transfer
+%                                     function error is maximized (recycles
+%                                     linear solves)
+%                           'avg':    Compute orth bases via a pivoted QR
+%                                     applied to prim bases; keep r <=
+%                                     q leading columns
+% @param proj:          What kind of projection do we use?
+%                           'g':      Primitive bases taken as Wrprim =
+%                                     Vrprim
+%                           'pg':     Primitive bases are computed
+%                                     differently; Wrprim encodes
+%                                     information of QO matrix Q
+% @param recomp_bases:  Bool; do we need to recompute primitive bases, or
+%                       are they passed as an arg?
+% @param recomp_tf:     Bool; do we need to recompute tf values, or
+%                       are they passed as an arg?
+% @param Vprim, Wprim:  Precommputed primitive bases
+% @param H_shifts:      Precommputed tf values
+%%
+% Default opts
+if ~isfield(opts, 'proj')
+    fprintf('Setting default value for opts.proj = g\n')
+    opts.proj = 'g'; % Galerkin projection
+end
+if ~isfield(opts, 'compression')
+    fprintf('Setting default value for opts.compression = avg\n')
+    opts.compression = 'avg'; % Orth via Pivoted QR
+end
+if ~isfield(opts, 'recomp_bases')
+    fprintf('Setting default value for opts.recomp_bases = 0\n')
+    opts.recomp_bases = 0; % False; recomp primitive bases
+end
+if ~isfield(opts, 'recomp_tf')
+    fprintf('Setting default value for opts.recomp_tf = 0\n')
+    opts.recomp_tf = 0; % False; recomp primitive bases
+end
+if ~isfield(opts, 'Vprim')
+    fprintf('Setting default value for opts.Vprim = []\n')
+    opts.Vprim = []; % No pre-computed bases
+end
+if ~isfield(opts, 'Wprim')
+    fprintf('Setting default value for opts.Wprim = []\n')
+    opts.Wprim = [];
+end
+if ~isfield(opts, 'H_shifts')
+    fprintf('Setting default value for opts.H_shifts = []\n')
+    opts.H_shifts = [];
+end
+
+q =  max(size(shifts));
+[n, ~] = size(A);
+if opts.recomp_bases % If we do compute the primitive bases
+    Vprim = zeros(n, q);   Wprim = zeros(n, q);
+    % Compute primitive bases
+    if strcmp(opts.proj, 'g')
+        % Primitive bases are identical as in Galerkin projection
+        linear_solves = tic;
+        fprintf('Computing model reduction bases via Galerkin projection \n')
+        for k = 1:q
+            tmp = (shifts(k) * E - A)\B;
+            Vprim(:, k) = tmp;  Wprim(:, k) = tmp;
+        end
+        fprintf('Vr and Wr computed in %.2f s\n', toc(linear_solves))
+        % Just to be safe ...
+        fprintf('Saving your g-bases!\n')
+        filename = 'prim_bases_g.mat';
+        save(filename, 'Vprim', 'Wprim');
+    end
+    if strcmp(opts.proj, 'pg')
+        % Primitive bases are different as in Petrov-Galerkin projection
+        % Left projection matrix encodes QO matrix
+        linear_solves = tic;
+        fprintf('Computing model reduction bases via Petrov-Galerkin projection \n')
+        for k = 1:q
+            tmp = (shifts(k) * E - A)\B;
+            Vprim(:, k) = tmp;
+            Wprim(:, k) = ((shifts(k) * E - A)' \ (Q * tmp));
+        end
+        fprintf('Vr and Wr computed in %.2f s\n', toc(linear_solves))
+        % Just to be safe ...
+        fprintf('Saving your pg-bases!\n')
+        filename = 'prim_bases_pg.mat';
+        save(filename, 'Vprim', 'Wprim');
+    end
+else % Bases are given in opts
+    fprintf('Primitive bases passed as args; not recomputing \n')
+    Vprim = opts.Vprim; Wprim = opts.Wprim;
+end
+
+if opts.recomp_tf
+    % Next, compute H at shifts; use precomputed solves
+    compute_tf = tic;
+    fprintf('Computing transfer function values at given shifts \n')
+    H_shifts = zeros(q, 1);
+    for k = 1:q
+        H_shifts(k) = Vprim(:, k)' * Q * Vprim(:, k);
+    end
+    fprintf('H_shifts computed in %.2f s\n', toc(compute_tf))
+else % Tf values given in opts
+    fprintf('Tf values passed as args; not recomputing \n')
+    H_shifts = opts.H_shifts;
+end
+
+if strcmp(opts.compression, 'avg')
+    compression = tic;
+    fprintf('Compressing projection bases via pivoted-QR \n')
+    [Vorth, ~, pV] = qr(Vprim, 'vector', 'econ');   [Worth, ~, pW] = qr(Wprim, 'vector', 'econ');
+    % Grab r `leading' columns of primitive bases according to pivoted QR
+    Vorth = Vorth(:, 1:r);   Worth = Worth(:, 1:r); % Double check this
+    fprintf('Vr and Wr orthonormalized in %.2f s\n', toc(compression))
+end
+if strcmp(opts.compression, 'Linfty')
+    overall_compression = tic;
+    fprintf('Compressing projection bases via greedy Linfty search \n')
+    % Choose r columns greedily where error is maximized
+    p = zeros(r, 1); % Space for indices chosen via greedy search
+    % First index is just max magnitude tf value, since `LQO-ROM' is zero
+    [~, p1] = max(abs(H_shifts));   
+    p(1) = p1;
+    % Project down
+    Er = Wprim(:, p1)' * E * Vprim(:, p1);  Ar = Wprim(:, p1)' * A * Vprim(:, p1); 
+    Qr = Vprim(:, p1)' * Q * Vprim(:, p1);  Br = Wprim(:, p1)' * B;
+    for k = 1:r-1
+        iter_compression = tic;
+        fprintf('Starting k = %d iteration of greedy Linfty search \n', k)
+        % Eval. LQO-ROM and compute error (order is k at each iter)
+        Hr_shifts = zeros(q, 1); % Eval at q shifts
+        for j = 1:q
+            tmpr = (shifts(j) * Er - Ar)\Br;
+            Hr_shifts(j) = Br'* (((shifts(j) * Er - Ar)') \ (Qr * tmpr)); 
+        end
+        Linfty_error = abs(H_shifts - Hr_shifts); % Compute Linfty_error
+        % Choose next index based on where error is biggest 
+        % Hacky, but set previously chosen indices to 0
+        for i = 1:k
+            Linfty_error(p(i)) = 0;
+        end
+        [~, pk] = max(Linfty_error);
+        p(k+1) = pk;
+        % QR compressed basis to avoid ill conditioning
+        [Worth, ~] = qr(Wprim(:, p(1:k+1)), 'econ');    [Vorth, ~] = qr(Vprim(:, p(1:k+1)), 'econ');
+        % Next Proj LQO-ROM; grab columns stored in P
+        Er = Worth' * E * Vorth;  Ar = Worth' * A * Vorth; 
+        Qr = Vorth' * Q * Vorth;  Br = Worth' * B;
+        fprintf('Search iteration done in %.2f s\n', toc(iter_compression))
+    end
+    % Worth = Wprim(:, p(1:k+1));    Vorth = Vprim(:, p(1:k+1));
+    pW = p; pV = p; % Save indices
+    fprintf('Vr and Wr orthonormalized in %.2f s\n', toc(overall_compression))
+end
+fprintf('Outputting\n')
+end
+
---
+++ ReiW24/drivers/sisolqo_irka.m
@@ -0,0 +1,194 @@
+function [E_r, A_r, b_r, c_r, M_r, poles, FOres, SOres, pole_history] = sisolqo_irka(...
+    E, A, b, c, M, poles_prev, FOres_prev, SOres_prev, itermax, eps, plot_conv)
+% Author: Sean Reiter (seanr7@vt.edu)
+
+% Apply the Iterative Rational Krylov Algorithm (IRKA) to Linear Quadratic
+% Output (LQO) systems.
+  
+% Paramters
+% ---------
+% @param (E, A, b, c, M) \in (Rnn, Rnn, Rn1, R1n, Rnn):
+%   State-space description of an n-dimensional LQO model. Assumed to be
+%   asymptotically stable.
+%   M is assumed to be symmetric; M = M'. 
+%   A is assumed to be diagonalizable.
+%   E (mass) is assumed to be nonsingular.
+
+% @param poles_prev:
+%   Initial selection of poles in open left half-plane.
+%   Stored as an (r x 1) array.
+%   Note: These are the `reduced-order poles' at initialization; 
+%   i.e., Ar = diag(L_prev).
+
+% @param FOres_prev, SOres_prev:
+%   Initial selection of 1st and 2nd-order (assumed real) residues.
+%   Stored as (r x 1) and (r x r) arrays, respectively. 
+%   Note: In the basis of A_r's eigenvectors, these are the linear and
+%   quadratic output matrices; i.e., c_r = FOres_prev, M_r = SOres_prev.
+
+% @param itermax:
+%   Max no. of iterations for LQO-IRKA to run. (Default itermax = 100)
+
+% @param eps:
+%   Convergence tol; if (max(eig(E_r, A_r) - L_prev) < eps), converged. 
+%   (Default eps = 10e-8)
+
+% @param plot_conv:
+%   Bool; do we plot the convergence of the reduced-order poles eig(E_r, A_r)?
+%   (Default plot_conv = False)
+
+% Outputs
+% ---------
+% @param (E_r, A_r, b_r, c_r, M_r) \in (Err, Rrr, Rr1, R1r, Rrr):
+%   State-space realization of the locally H2-optimal r-dimensional
+%   LQO-ROM, obtained via Petrov-Galerkin (PG) projection.
+
+% @param pole_history:
+%   History of interpolation points, as an (r x iter) array.
+
+% @param FOres, SOres:
+%   Converged 1st and 2nd-order residues, as (r x 1) and (r x r) arrays.
+
+%% LQO-IRKA
+% Take input data; Compute initial LQO-ROM via PG-proj
+r = length(poles_prev);     [~, n] = size(A); % ROM and FOM dimensions
+
+% Check iteration/convergence tolerances; set to defaults if unspecified
+if nargin == 10 % (Default is to not plot convergence of the RO-poles)
+    plot_conv = 0;
+end
+if nargin == 9 % (No convergence tolerance set)
+    eps = 10e-8;
+end
+if nargin == 8 % (No max number of iterations set)
+    itermax = 100;
+end
+
+% If no c term or residues inputted; output is purely quadratic
+pure_QO = false;
+if isempty(c)
+    pure_QO = true;
+    c_r = [];   FOres = []; % Dummy arrays so error not thrown
+end
+
+% Start the clock on the total iteration
+overall_timer_start = tic;
+fprintf('Beginning IRKA iteration\n')
+
+% Initialize poles + residues
+poles = poles_prev; FOres = FOres_prev; SOres = SOres_prev; 
+pole_history(:, 1) = poles;
+
+% Counter + tolerance to enter while
+iter = 1;   err(iter) = eps + 1; 
+while (err(iter) > eps && iter <= itermax)
+    % Pre-allocate space for right, left PG-proj bases V_r, W_r
+    % (Note: Construction requires RO-poles and residues @ each iter)
+    V_r = zeros(n, r);     W_r = zeros(n, r);
+
+    % Start the clock
+    thisiter_timer_start = tic;
+    fprintf('Beginning next IRKA iteration; current iterate is k = %d\n', iter)
+
+    % First, fill out columns of Vr; used in computing Wr
+    k = 1;
+    while k <= r
+        % 1. Construction of Vr enforces r + r^2 interpolation conditions:
+        %   @math: H1(poles(k)) = H1r(poles(k)), k = 1, ..., r
+        %   @math: H2(poles(i), poles(j)) = H2(poles(i), poles(j)), 
+        %           i, j = 1, ..., r 
+        V_r(:, k) = ((-poles(k)) * E - A)\b; 
+        k = k + 1;
+    end
+
+    % Now, fill out colummns of W_r
+    k = 1; 
+    while k <= r
+        % 2. Construction of W_r enforces r `mixed' Hermite conditions:
+        %   @math: FOres(k) * H1^(1)(poles(k)) + 2 * \sum_{j = 1}^{r} ...
+        %          SOres(k, j) * H2^(1, 0)(-poles(k), -poles(j)) = ...
+        %          FOres(k) * H1_r^(1)(poles(k)) + 2 * \sum_{j = 1}^{r} ...
+        %          SOres(k, j) * H2_r^(1, 0)(poles(k), poles(j)) = ...
+        tmp = zeros(n, 1); 
+        i = 1;
+        while i <= r 
+            % Compute sum over i of mu_i,k * (poles(i) * E - A)\b
+            tmp = tmp + (SOres(i, k) * V_r(:, i)); 
+            i = i + 1;
+        end
+        % Multiply by - 2 * M
+        tmp = - 2 * M * tmp; 
+        if ~pure_QO % If not purely QO, compute 
+            tmp = tmp - FOres(k) *  c';
+        end
+        % Now, only one final linear solve required; apply to the sum
+        % TODO: Need conj of pole here? I don't think so, just the negative
+        % W_r(:, k) = ((-poles(k) * E' - A')\tmp);
+        W_r(:, k) = ((-poles(k) * E - A)'\tmp);
+        k = k + 1;
+    end
+    
+    % Orthonormalize projection matrices
+    [V_r, ~] = qr(V_r, "econ");     [W_r, ~] = qr(W_r, "econ");
+    % Compute LQO-ROM via PG-proj and new parameters
+    % (1-31-24) Change to real transpose instead of Hermitian; may need to put back! - SR
+    % W_rt = W_r.';  V_rt = V_r.';
+    W_rt = W_r';  V_rt = V_r';
+    E_r = W_rt * E * V_r;   A_r = W_rt * A * V_r;   b_r = W_rt * b;
+    if ~pure_QO % Compute reduced linear output term
+        c_r = c * V_r;  
+    end
+    M_r = V_rt * M * V_r;
+
+    % Save params from prev iteration
+    poles_prev = poles; SOres_prev = SOres;
+    
+    % Diagonalize Ar; Get RO-poles ...
+    [X_r, L_r] = eig(A_r, E_r);     poles = diag(L_r);
+    try
+        poles = cplxpair(poles, 10e-3); % Sort in complex conj pairs
+    catch
+        warning('Warning! Reduced-model has become overtly complex; cannot sort eigenvalues into complex conjugate pairs. Returning unsorted poles')
+    end
+    pole_history(:,iter+1) = poles;
+
+    % ... + 1st, 2nd-order residues of H1r, H2r
+    X_rinv = X_r\eye(r, r); mod_b_r = E_r\b_r;
+    if ~pure_QO % Compute FO residues
+        FOres_prev = FOres; 
+        FOres = (c_r * X_r)' .* (X_rinv * (mod_b_r));    
+    end
+    SOres = ((X_rinv * (mod_b_r))' .* ((X_r' * M_r * X_r))) .* (X_rinv * (mod_b_r));
+
+    % End the clock
+    fprintf('End of current IRKA iterate k = %d\n', iter)
+    fprintf('Current IRKA iteration finished in %.2f s\n',toc(thisiter_timer_start))
+
+    % Track convergence of poles; Update iter count + compute max deviation 
+    % between poles across iterations
+    iter = iter + 1;    err(iter) = max(abs(poles - poles_prev));
+    fprintf('Largest magnitude change in interpolation points from previous iteration is %.2f \n', err(iter))
+end
+
+% If broken, interpolation occurs using poles + residues from previous iter
+% Return appropriate poles + residues
+poles = poles_prev; SOres = SOres_prev;
+if ~pure_QO
+    FOres = FOres_prev;
+end
+
+if iter == (itermax + 1)
+    fprintf('IRKA has terminated due to reaching the max no. of iterations; total time elapsed is %.2f s\n', toc(overall_timer_start))
+else
+    fprintf('IRKA has converged in %d iterations\n', iter)
+    fprintf('Total time elapsed is %.2f s\n', toc(overall_timer_start))
+end
+
+% If plotting convergence
+if plot_conv == true
+    semilogy(1:iter, err, '-o', LineWidth=1.5)
+    xlim([1,iter])
+    xlabel('Iteration count')
+    ylabel('Magnitude of change in \lambda')
+end
+end
