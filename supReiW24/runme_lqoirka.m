d1dd827d40d3c440a5c53c75f19a32f4b7ad4c0f
seanr7 <seanr7@vt.edu>
2024-04-10 10:02:57 -0400

    save file

--- supReiW24/runme_lqoirka.m
+++ supReiW24/runme_lqoirka.m
@@ -95,7 +95,7 @@ opts.fores = [];
 
 poles = info.pole_hist;
 SOres = info.sores; 
-filename = 'results/plateTVAlqo_r50_lqoirka.mat';
+filename = 'results/plateTVAlqo_r50_full_lqoirka.mat';
 save(filename, 'E_qo_r', 'A_qo_r', 'B_qo_r', 'Q_qo_r', 'poles', 'SOres') 
 
 %% Finished script.
