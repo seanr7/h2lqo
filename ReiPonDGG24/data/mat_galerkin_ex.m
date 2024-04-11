d1438079a1dd4a3a799b11a782aff7060c7bc93f
seanr7 <seanr7@vt.edu>
2024-04-11 11:49:44 -0400

    rebase

--- ReiPonDGG24/data/mat_galerkin_ex.m
+++ ReiPonDGG24/data/mat_galerkin_ex.m
@@ -1,4 +1,4 @@
-function [A,B,M] = mat_galerkin_ex(degr,sigma)
+function [A,B,Mquad] = mat_galerkin_ex(degr,sigma)
 
 % evaluation of Galerkin system with gPC coefficients as output
 [Ahat,Bhat,Chat,Ehat] = matrices_galerkin(degr,sigma);
@@ -11,7 +11,12 @@ B = Ehat\Bhat;
 % matrix-matrix product (used twice)
 % first row of Chat cut off
 Ccut = Chat(2:end,:);
-M = Ccut'*Ccut;
+% Mquad1 = zeros(dim, dim);
+% Mquad2 = Ccut'*Ccut;
+% Clin = [Chat(1,:); zeros(1,dim)];
+
+Mquad = Ccut'*Ccut;
+
 
 end
 
