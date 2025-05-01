fd2410daeeeec0b2f745ecf6d1d14fa85e2a6d15
seanr7 <seanr7@vt.edu>
2025-05-01 14:49:03 -0400

    lqo bt

--- supReiGPG24/drivers/miso_lqobt.m
+++ supReiGPG24/drivers/miso_lqobt.m
@@ -1,6 +1,10 @@
-function [Er, Ar, Br, cr, Mr, info] = miso_lqobt(E, A, B, c, M, r)
+function [Ar, Br, cr, Mr, info] = miso_lqobt(A, B, c, M, r)
 % MISO_LQOBT Balanced truncation algorithm for model-order reudction of
 % linear systems with multiple quadratic outputs
+%
+% SYNTAX:
+%   [Ar, Br, cr, Mr, info] = miso_lqobt(A, B, c, M, r, opts)
+%   [Ar, Br, cr, Mr]       = miso_lqobt(A, B, c, M, r)
 %
 % DESCRIPTION:
 %   Computes a linear quadratic output reduced model (Ar, Br, Cr, M1r, ...,
@@ -12,13 +16,13 @@ function [Er, Ar, Br, cr, Mr, info] = miso_lqobt(E, A, B, c, M, r)
 %   Square root factors of the quadratic output system Gramians P = U*U'
 %   and Q = L*L', which satisfy
 %
-%       A*P*E' + E*P*A' + B*B' = 0                               (1)
-%       A'*Q*E + E'*Q*A + c*c' + M*P*M= 0   (2)
+%       A*P  + P*A' + B*B' = 0                               (1)
+%       A'*Q + Q*A  + c*c' + M*P*M= 0   (2)
 %
-%   are solved for. A singular value decomposition (svd) of U'*E*L is
+%   are solved for. A singular value decomposition (svd) of U'L is
 %   computed
 %
-%       U'*E*L = [Z1, Z2] * diag(S1, S2) * [Y1, Y2]'          (2)
+%       U'L = [Z1, Z2] * diag(S1, S2) * [Y1, Y2]'          (2)
 %
 %   Where the matrices in the svd are partitioned according to the
 %   reduction order 1 < r <= n. A reduced order model is obtained using the
@@ -27,18 +31,16 @@ function [Er, Ar, Br, cr, Mr, info] = miso_lqobt(E, A, B, c, M, r)
 %       Vr = U*Z1*S1^(-1/2) and Wr = L*Y1*S1^(-1/2)         (3)
 %
 %   It is assumed that the eigenvalues of (s*E - A) lie in the open left
-%   half-plane, and that E is nonsingular.
+%   half-plane.
 %
 % INPUTS:
-%   E    - descriptor matrix with dimensions n x n in (1)
 %   A    - state matrix with dimensions n x n in (1)
 %   B    - input matrix with dimensions n x m in (1)
 %   c    - linear output matrix with dimensions n x 1 in (2)
 %   M    - (symmetric) quadratic output matrix n x n
 %   r    - order of reduction
 % 
-% OUTPUTS:
-%   Er    - reduced descriptor matrix with dimensions r x r
+% OUTPUTS: 
 %   Ar    - reduced state matrix with dimensions r x r 
 %   Br    - reduced descriptor matrix with dimensions r x m 
 %   cr    - reduced linear output matrix with dimensions r x 1 
@@ -57,12 +59,12 @@ function [Er, Ar, Br, cr, Mr, info] = miso_lqobt(E, A, B, c, M, r)
 %
 
 %
-% Copyright (c) 2024 Sean Reiter
+% Copyright (c) 2025 Sean Reiter
 % All rights reserved.
 % License: BSD 2-Clause license (see COPYING)
 %
 % Virginia Tech, USA
-% Last editied: 3/27/2024
+% Last editied: 4/30/2024
 %
 
 %%
@@ -70,8 +72,6 @@ function [Er, Ar, Br, cr, Mr, info] = miso_lqobt(E, A, B, c, M, r)
 % CHECK INPUTS.                                                           %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 n = size(A, 1);
-m = size(B, 2);
-p = 1;
 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -83,16 +83,14 @@ lyapTime = tic;
 fprintf(1, 'SOLVING FOR GRAMIANS FROM LYAPUNOV EQUATIONS.\n')
 fprintf(1, '---------------------------------------------\n');
 % Solve for reachability Gramian P.
-% P = lyap(A, B*B', [], E);
-% U = mess_lyap(A, B, [], [], E);
+P = lyap(A, B*B');
 
 % Solve for quadratic output observability Gramian Q.
-% L = mess_lyap(A', [c, M*U], [], [], E');
-% Q = lyap(A', (c*c' + M*P*M), [], E');
+Q = lyap(A', (c*c' + M*P*M));
 fprintf(1, 'GRAMIANS SOLVED FOR IN %.2f s\n', toc(lyapTime))
 
 fprintf(1, 'COMPUTING PROJECTION MATRICES.\n')
-fprintf(1, '---------------------------- -\n');
+fprintf(1, '------------------------------\n');
 
 % Compute square root factors of Gramians, and take the svd of U'*L.
 try
@@ -109,14 +107,13 @@ catch
     L = chol(Q + eye(n, n)*10e-10);    
     L = L';
 end
-[Z, S, Y] = svd(U'*E*L);
+[Z, S, Y] = svd(U'*L);
 
 % Compute projection matrices.
 V = U*Z(:, 1:r)*S(1:r, 1:r)^(-1/2); % Right
 W = L*Y(:, 1:r)*S(1:r, 1:r)^(-1/2);% Left
 
 % Compute reduced order model via projection.
-Er  = eye(r, r);
 Ar  = W.'*A*V;   Br = W.'*B;   
 % Linear output.
 cr = V.'*c;
